5a541030a438e11f70418c832b7fa0e1da5af2fb
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-11-30 16:24:34 +0100

    route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Starting a web FLASK application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return 'C %s' % mod_text.replace("_", " ")
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is cool"):
+    return 'Python %s' % mod_text.replace("_", " ")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
