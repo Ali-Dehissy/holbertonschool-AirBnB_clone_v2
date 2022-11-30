e2cabfa66fe20b20d5fd826c0b5511c8a3caa0d0
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-11-30 16:54:11 +0100

    number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Starting a web FLASK application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return 'C {:s}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text="is_cool"):
+    return 'Python %s' % text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def python_number(n):
+    return '%d is a number' % n
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
